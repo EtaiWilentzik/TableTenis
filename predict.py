028fbe975e73d58a988905418a434dae3ce00e70
ADI <adi.tal.25.12@gmail.com>
2024-01-17 14:20:39 +0200

    changed ball and table classes

--- predict.py
+++ predict.py
@@ -1,24 +1,16 @@
 import os
-
 from ultralytics import YOLO
-
 import cv2
 import time
-
+from Ball import Ball
+from Table import Table
 
 # when the y in the image is more high its more small in the code (like arkanoid)
-def bounce(posList, frame):
-    if (len(posList) < 3):
-        return
-    # y1table<posList[-2][1]<y2table:
-    if posList[-1][1] < posList[-2][1] and posList[-2][1] > posList[-3][1] and x2table > posList[-2][0] > x1table and (
-            y1table - 100) < posList[-2][1] < y2table:
-        posList[-2] = (posList[-2][0], posList[-2][1], True)
 
 
 VIDEOS_DIR = os.path.join('.', 'videos')
 
-video_path = os.path.join(VIDEOS_DIR, 'v1.mp4')
+video_path = os.path.join(VIDEOS_DIR, 'short.mp4')
 video_path_out = '{}_out.mp4'.format(video_path)
 
 cap = cv2.VideoCapture(video_path)
@@ -32,11 +24,12 @@ model_path = os.path.join('.', 'train8', 'weights', 'last.pt')
 
 # Load a model
 model = YOLO(model_path)  # load a custom model
-counterFrame = 420
+counterFrame = 60
 counterUntilFrame = 0
 x1table, y1table, x2table, y2table = 0, 0, 0, 0
 threshold = 0.5
-posList = []
+ball = Ball()
+table = Table()
 while ret:
     start_time = time.time()
     # max_det=3  in model.predict say how many object i want to detect
@@ -48,25 +41,26 @@ while ret:
         yCenter = (int)((int(y1) + int(y2)) / 2)
 
         if score > threshold:
-            if (class_id == 2 and counterUntilFrame == counterFrame):
-                x1table, y1table, x2table, y2table = x1, y1, x2, y2
+            if class_id == 2 and counterUntilFrame == counterFrame:
+                table.set_bottom_right((x2, y2))
+                table.set_top_left((x1, y1))
 
-            if (class_id == 0):
-                posList.append((xCenter, yCenter, False))
-                # top left is first, bottom right is second, color is third, and thicknes is the last
-            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+            if class_id == 0:
+                ball.set_coordinates(xCenter, yCenter)
+                # top left is first, bottom right is second, color is third, and thickness is the last
+            cv2.rectangle(frame, table.get_top_left(), table.get_bottom_right(), (0, 255, 0), 4)
             cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
                         cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA, )
-            if len(posList) > 30:
-                posList.pop(0)
-            bounce(posList, frame)
-            for i, pos in enumerate(posList):
+            ball.bounce(table)
+            tmp_positions = ball.get_positions()
+            for i, pos in enumerate(tmp_positions):
                 if pos[2]:
                     cv2.circle(frame, (pos[0], pos[1]), 15, (0, 0, 255), cv2.FILLED)
                 else:
                     cv2.circle(frame, (pos[0], pos[1]), 5, (0, 255, 0), cv2.FILLED)
                 if i != 0:
-                    cv2.line(frame, (pos[0], pos[1]), (posList[i - 1][0], posList[i - 1][1]), (0, 0, 255), 2)
+                    cv2.line(frame, (pos[0], pos[1]), (tmp_positions[i - 1][0], tmp_positions[i - 1][1]), (0, 0, 255),
+                             2)
 
     out.write(frame)
     elapsed_time_ms = (time.time() - start_time) * 1000
